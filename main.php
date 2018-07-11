5ebfad4e814683a761b490975910ce7d7725a5ef
ifer777 <ivan.paz@itsae.edu.ec>
2018-07-10 23:37:47 -0500

    main_view

---
+++ main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Elecciones 2018 CADE</title>
+	<link rel="stylesheet" href="">
+	<?php include('includes/link.html'); ?>
+</head>
+<body>
+	<div class="container" style="margin-top: 100px;">
+		<div class="row">
+			<div class="col s12 m6">
+				<div class="team-wrapper">
+                      <div class="team-img">
+                      		<h5 align="center">Lista A</h5>
+                          <a href="#"><img src="assets/img/team/team-7.jpg" class="img-responsive" alt="Image"></a>
+                      </div><!-- /.team-img -->
+
+                      <div class="team-title">
+                          <h3><a href="#">Jose Cevallos</a></h3>
+                          <span>Presidente</span>
+                          <p>. </p>
+                          <form action="#">
+                          	<p>
+      							<label>
+        							<input type="checkbox" />
+        							<span>Tu voto</span>
+      							</label>
+    						</p>
+                          </form>
+                      </div><!-- /.team-title -->
+                  </div>
+			</div>
+			<div class="col s12 m6">
+				<div class="team-wrapper">
+                      <div class="team-img">
+                      	<h5 align="center">Lista B</h5>
+                          <a href="#"><img src="assets/img/team/team-7.jpg" class="img-responsive" alt="Image"></a>
+                      </div><!-- /.team-img -->
+
+                      <div class="team-title">
+                          <h3><a href="#">Allison Zambrano</a></h3>
+                          <span>Presidente</span>
+                          <p>. </p>
+                          <form action="#">
+                          	<p>
+      							<label>
+        							<input type="checkbox" />
+        							<span>Tu voto</span>
+      							</label>
+    						</p>
+                          </form>
+                      </div><!-- /.team-title -->
+                  </div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
