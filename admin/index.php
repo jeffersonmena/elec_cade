016413bdc7166fa5e6932c2501fe5b753b7d5462
jeffersonmena <jefferson.mena@itsae.edu.ec>
2018-07-13 02:52:49 -0500

    reportes -> admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -10,36 +10,17 @@
   <meta name="msapplication-tap-highlight" content="no">
   <meta name="description" content="ELECCIONES CADE ">
   <meta name="keywords" content="ELECCIONES">
+  <meta http-equiv="refresh" content="1">
   <title>CADE Elige</title>
   <?php include('includes/links.php'); ?>
 </head>
-
 <body>
-  <!-- //////////////////////////////////////////////////////////////////////////// -->
-
-  <!-- START HEADER -->
     <?php include('includes/header.php'); ?>
-  <!-- END HEADER -->
-
-  <!-- //////////////////////////////////////////////////////////////////////////// -->
-
-  <!-- START MAIN -->
   <div id="main">
-    <!-- START WRAPPER -->
     <div class="wrapper">
-
-      <!-- START LEFT SIDEBAR NAV-->
       <?php include('includes/aside.php'); ?>
-      <!-- END LEFT SIDEBAR NAV-->
-
-      <!-- //////////////////////////////////////////////////////////////////////////// -->
-
-      <!-- START CONTENT -->
       <section id="content">
-        
-        <!--breadcrumbs start-->
         <div id="breadcrumbs-wrapper">
-            <!-- Search for small screen -->
             <div class="header-search-wrapper grey hide-on-large-only">
                 <i class="mdi-action-search active"></i>
                 <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Filtrar Resultados">
@@ -57,20 +38,40 @@
             </div>
           </div>
         </div>
-        <!--breadcrumbs end-->
-        
-        <div id="card-stats">
+                    <div id="card-stats">
                         <div class="row">
                             <div class="col s12 m6 l3">
                                 <div class="card">
-                                    <div class="card-content  green white-text">
-                                        <p class="card-stats-title"><i class="mdi-social-group-add"></i>Total Estudiantes</p>
-                                        <h4 class="card-stats-number">
-                                          <?php 
+                                    <div class="card-content blue-grey white-text">
+                                        <p class="card-stats-title"><i class="mdi-social-group-add"></i> 
+                                          Total Estudiantes                                          
+                                        </p>
+                                        <h4 class="card-stats-number"> 
+                                        <?php 
                                            $cant_stud=mysqli_query($con,"SELECT id_estudiante FROM estudiantes ");
                                             $cant_studs=mysqli_num_rows($cant_stud);
                                            echo $cant_studs;
-                                         ?>
+                                         ?>                                                                                 
+                                        </h4>
+                                        <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 0% <span class="blue-grey-text text-lighten-5">from TODAY</span>
+                                        </p>
+                                    </div>
+                                    <div class="card-action blue-grey darken-2">
+                                        <div id="profit-tristate" class="center-align"><canvas width="227" height="25" style="display: inline-block; width: 227px; height: 25px; vertical-align: top;"></canvas></div>
+                                    </div>
+                                </div>
+                            </div>                          
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  green white-text">
+                                        <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i>
+                                        LISTA " A "</p>
+                                        <h4 class="card-stats-number">
+                                          <?php 
+                                           $cant_stud=mysqli_query($con,"SELECT voto, COUNT(*)as num FROM votos WHERE voto='A'");
+                                            $cont=mysqli_fetch_assoc($cant_stud);
+                                           echo $Total=$cont['num'];
+                                         ?>                                          
                                          </h4>
                                         <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 100% <span class="green-text text-lighten-5">from TODAY</span>
                                         </p>
@@ -83,13 +84,15 @@
                             <div class="col s12 m6 l3">
                                 <div class="card">
                                     <div class="card-content pink lighten-1 white-text">
-                                        <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i> LISTA " A "</p>
+                                        <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i> 
+                                          LISTA " B "
+                                        </p>
                                         <h4 class="card-stats-number">
                                           <?php 
-                                           $cant_stud=mysqli_query($con,"SELECT voto, COUNT(*)as num FROM votos WHERE voto='A'");
+                                           $cant_stud=mysqli_query($con,"SELECT voto, COUNT(*)as num FROM votos WHERE voto='B'");
                                             $cont=mysqli_fetch_assoc($cant_stud);
                                            echo $Total=$cont['num'];
-                                         ?>
+                                         ?> 
                                         </h4>
                                         <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-down"></i> 0% <span class="deep-purple-text text-lighten-5">from TODAY</span>
                                         </p>
@@ -99,25 +102,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col s12 m6 l3">
-                                <div class="card">
-                                    <div class="card-content blue-grey white-text">
-                                        <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i> LISTA " B "</p>
-                                        <h4 class="card-stats-number">
-                                          <?php 
-                                           $cant_stud=mysqli_query($con,"SELECT voto, COUNT(*)as num FROM votos WHERE voto='B'");
-                                            $cont=mysqli_fetch_assoc($cant_stud);
-                                           echo $Total=$cont['num'];
-                                         ?>                                          
-                                        </h4>
-                                        <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 0% <span class="blue-grey-text text-lighten-5">from TODAY</span>
-                                        </p>
-                                    </div>
-                                    <div class="card-action blue-grey darken-2">
-                                        <div id="profit-tristate" class="center-align"><canvas width="227" height="25" style="display: inline-block; width: 227px; height: 25px; vertical-align: top;"></canvas></div>
-                                    </div>
-                                </div>
-                            </div>
+
                             <div class="col s12 m6 l3">
                                 <div class="card">
                                     <div class="card-content purple white-text">
@@ -138,16 +123,38 @@
                                 </div>
                             </div>
                         </div>
+                      <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content #0288d1 light-blue darken-2 white-text">
+                                        <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i> Estudiantes Restantes</p>
+                                        <h4 class="card-stats-number">
+                                          <?php 
+                                           $cant_s=mysqli_query($con,"SELECT id_estudiante FROM estudiantes ");
+                                           $cant_1=mysqli_num_rows($cant_s);
+                                           $cant_v=mysqli_query($con,"SELECT id_voto FROM votos ");
+                                           $cant_2=mysqli_num_rows($cant_v);
+                                           $faltantes=$cant_1-$cant_2;
+                                          echo $faltantes;
+
+                                         ?>  
+                                        </h4>
+                                        <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-down"></i> 0% <span class="deep-purple-text text-lighten-5">from TODAY</span>
+                                        </p>
+                                    </div>
+                                    <div class="card-action  #0277bd light-blue darken-3 darken-2">
+                                        <div id="invoice-line" class="center-align"><canvas width="223" height="25" style="display: inline-block; width: 223px; height: 25px; vertical-align: top;"></canvas></div>
+                                    </div>
+                                </div>
+                            </div>                        
+                      </div>                        
                     </div>
-        <!--start container-->
         <div class="container">
           <div class="section">
-
             <p class="caption"></p>
             <div class="divider"></div>
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
           </div>
-          <!-- Floating Action Button -->
             <div class="fixed-action-btn" style="bottom: 50px; right: 19px;">
                 <a class="btn-floating btn-large">
                   <i class="mdi-action-stars"></i>
@@ -159,27 +166,11 @@
                   <li><a href="app-email.html" class="btn-floating blue"><i class="large mdi-communication-email"></i></a></li>
                 </ul>
             </div>
-            <!-- Floating Action Button -->
         </div>
-        <!--end container-->
       </section>
-      <!-- END CONTENT -->
-
-      <!-- //////////////////////////////////////////////////////////////////////////// -->
-      <!-- START RIGHT SIDEBAR NAV-->
         <?php include('includes/right_sidebar.php'); ?>
-      <!-- LEFT RIGHT SIDEBAR NAV-->
-
     </div>
-    <!-- END WRAPPER -->
-
   </div>
-  <!-- END MAIN -->
-
-
-  <!-- //////////////////////////////////////////////////////////////////////////// -->
-
-  <!-- START FOOTER -->
   <footer class="page-footer" style="background-color: #1ba0b1;">
     <div class="footer-copyright">
       <div class="container">
@@ -188,30 +179,11 @@
         </div>
     </div>
   </footer>
-  <!-- END FOOTER -->
-
-
-
-    <!-- ================================================
-    Scripts
-    ================================================ -->
-    
-    <!-- jQuery Library -->
     <script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>    
-    <!--materialize js-->
     <script type="text/javascript" src="js/materialize.js"></script>
-    <!--prism
-    <script type="text/javascript" src="js/prism/prism.js"></script>-->
-    <!--scrollbar-->
     <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <!-- chartist -->
     <script type="text/javascript" src="js/plugins/chartist-js/chartist.min.js"></script>   
-    
-    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
     <script type="text/javascript" src="js/plugins.js"></script>
-    <!--custom-script.js - Add your own theme custom JS-->
     <script type="text/javascript" src="js/custom-script.js"></script>
-    
 </body>
-
 </html>
